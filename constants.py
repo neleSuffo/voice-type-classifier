432f2aa46b93f2574c82a5737f13100190cd85cb
NeleSuffo <nele.suffo@leuphana.de>
2025-10-19 11:27:14 +0200

    refactor constants.py to organize file paths and class structure for VoiceTypeClassifier and AudioClassification

--- constants.py
+++ constants.py
@@ -1,20 +1,37 @@
-class VTC:
-    voice_types_list = ['KCHI', 'CHI', 'FEM', 'MAL', 'SPEECH']
-
+from pathlib import Path
+class VoiceTypeClassifier:
+    CHILDLENS_CLASSES = ['KCHI', 'CHI', 'FEM', 'MAL', 'SPEECH']
+    OUTPUT_DIR = Path("/home/nele_pauline_suffo/outputs")
+    
     # Define constants and file paths
-    childlens_gt_df_file_path = "/home/nele_pauline_suffo/ProcessedData/vtc_childlens/annotations_gt_id_split.pkl"
+    CHILDLENS_GT_FILE_PATH = "/home/nele_pauline_suffo/ProcessedData/vtc_childlens/annotations_gt_id_split.pkl"
+    UEM_CHILDLENS_FILE_PATH = "/home/nele_pauline_suffo/ProcessedData/vtc_childlens/complete.uem"
+    
+    VTC_OG_DIR = OUTPUT_DIR / "vtc"
+    RTTM_OG_01_FILE_PATH = VTC_OG_DIR / "childlens_audio_test_01/all.rttm"
+    RTTM_OG_2_FILE_PATH = VTC_OG_DIR / "childlens_audio_test_2/all.rttm"
+    OUTPUT_OG_01_FILE_PATH = VTC_OG_DIR / "childlens_og_test_01.pkl"
+    OUTPUT_OG_2_FILE_PATH = VTC_OG_DIR / "childlens_og_test_2.pkl"
 
-    rttm_og_01_file_path = "/home/nele_pauline_suffo/outputs/vtc/childlens_audio_test_01/all.rttm"
-    rttm_og_2_file_path = "/home/nele_pauline_suffo/outputs/vtc/childlens_audio_test_2/all.rttm"
-    output_og_01_file_path = "/home/nele_pauline_suffo/outputs/vtc/childlens_og_test_01.pkl"
-    output_og_2_file_path = "/home/nele_pauline_suffo/outputs/vtc/childlens_og_test_2.pkl"
-
-    rttm_ft_01_file_path = "/home/nele_pauline_suffo/outputs/vtc_finetuned/childlens_audio_test_01/all.rttm"
-    rttm_ft_2_file_path = "/home/nele_pauline_suffo/outputs/vtc_finetuned/childlens_audio_test_2/all.rttm"
-    output_ft_01_file_path = "/home/nele_pauline_suffo/outputs/vtc_finetuned/childlens_ft_test_01.pkl"
-    output_ft_2_file_path = "/home/nele_pauline_suffo/outputs/vtc_finetuned/childlens_ft_test_2.pkl"
-
-    rttm_cl_01_file_path = "/home/nele_pauline_suffo/outputs/vtc_from_scratch/childlens_audio_test_01/all.rttm"
-    rttm_cl_2_file_path = "/home/nele_pauline_suffo/outputs/vtc_from_scratch/childlens_audio_test_2/all.rttm"
-    output_cl_01_file_path = "/home/nele_pauline_suffo/outputs/vtc_from_scratch/childlens_cl_test_01.pkl"
-    output_cl_2_file_path = "/home/nele_pauline_suffo/outputs/vtc_from_scratch/childlens_cl_test_2.pkl"
+    VTC_FINETUNED_DIR = OUTPUT_DIR / "vtc_finetuned"
+    RTTM_FT_01_FILE_PATH = VTC_FINETUNED_DIR / "childlens_audio_test_01/all.rttm"
+    RTTM_FT_2_FILE_PATH = VTC_FINETUNED_DIR / "childlens_audio_test_2/all.rttm"
+    OUTPUT_FT_01_FILE_PATH = VTC_FINETUNED_DIR / "childlens_ft_test_01.pkl"
+    OUTPUT_FT_2_FILE_PATH = VTC_FINETUNED_DIR / "childlens_ft_test_2.pkl"
+    
+    VTC_FROM_SCRATCH_DIR = OUTPUT_DIR / "vtc_from_scratch"
+    RTTM_CL_01_FILE_PATH = "/home/nele_pauline_suffo/outputs/vtc_from_scratch/childlens_audio_test_01/all.rttm"
+    RTTM_CL_2_FILE_PATH = "/home/nele_pauline_suffo/outputs/vtc_from_scratch/childlens_audio_test_2/all.rttm"
+    OUTPUT_CL_01_FILE_PATH = "/home/nele_pauline_suffo/outputs/vtc_from_scratch/childlens_cl_test_01.pkl"
+    OUTPUT_CL_2_FILE_PATH = "/home/nele_pauline_suffo/outputs/vtc_from_scratch/childlens_cl_test_2.pkl"
+    
+class AudioClassification:
+    QUANTEX_CLASSES = ['KCHI', 'KCDS', 'OHS']
+    QUANTEX_GT_FILE_PATH = "/home/nele_pauline_suffo/ProcessedData/audio_cls_input/annotations_gt_id_split.pkl"
+    UEM_QUANTEX_FILE_PATH = "/home/nele_pauline_suffo/ProcessedData/audio_cls_input/complete.uem"
+    
+    RTTM_QUANTEX_01_FILE_PATH = "/home/nele_pauline_suffo/projects/voice-type-classifier/output_voice_type_classifier/childlens_audio_three_classes_test/all.rttm"
+    RTTM_QT_01_FILE_PATH = "/home/nele_pauline_suffo/projects/voice-type-classifier/output_voice_type_classifier/childlens_audio_three_classes_test_01/all.rttm"
+    RTTM_QT_2_FILE_PATH = "/home/nele_pauline_suffo/projects/voice-type-classifier/output_voice_type_classifier/childlens_audio_three_classes_test_2/all.rttm"
+    OUTPUT_QT_01_FILE_PATH = "/home/nele_pauline_suffo/outputs/audio_classification/vtc_three_classes_test_01.pkl"
+    OUTPUT_QT_2_FILE_PATH = "/home/nele_pauline_suffo/outputs/audio_classification/vtc_three_classes_test_2.pkl"
